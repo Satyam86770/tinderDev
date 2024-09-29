d49e459f378719d92f8384503baed977fa1b31e7
Satyam86770 <satyam86770@gmail.com>
2024-09-29 13:48:32 +0530

    learn about different http method get, post, use, etc

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ app.listen(7777,()=>{
     console.log("Server is listening on port 7777");
  });
 
-app.use("/test",(req,res)=>{
-   res.send("Hi I finally make my new Server");
+app.get("/user",(req,res)=>{
+   console.log(req.query);
+   res.send("Hi You Landed to Get Test");
 })
+
+
+app.post("/test",(req,res)=>{
+   res.send("Hi You Landed to Post Test");
+})
+
